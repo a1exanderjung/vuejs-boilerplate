d4f64a0a1bbaf9027361814e414e7f3837ecdfb4
Alexander Jung <alexander@jung.net>
2018-01-03 18:31:42 +0000

    Lowercase app and comments

--- template/app/main.js
+++ template/app/main.js
@@ -1,9 +1,18 @@
+/*
+ * Highly opinionated VueJS boilerplate ({{ template_version }})
+ *
+ * @author  Alexander Jung <alexander@jung.net>
+ */
+
+'use strict'
+
 {{#if_eq build "standalone"}}
 // The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// (runtime-only or standalone) has been set in config/default.js with an alias.
+
 {{/if_eq}}
 import Vue from 'vue'
-import App from './App'
+import app from './app'
 {{#router}}
 import router from './router'
 {{/router}}
@@ -17,10 +26,10 @@ new Vue({
   router,
   {{/router}}
   {{#if_eq build "runtime"}}
-  render: h => h(App)
+  render: h => h(app)
   {{/if_eq}}
   {{#if_eq build "standalone"}}
-  components: { App },
-  template: '<App/>'
+  components: { app },
+  template: '<app/>'
   {{/if_eq}}
 })
